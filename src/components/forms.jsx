a762b5fcbcf0dd9798bbb3cc42252b7f909493ee
eitan <eitanytsur@gmail.com>
2025-02-06 13:25:52 +0200

    Update form iframe source and add background color to form container

--- src/components/forms.jsx
+++ src/components/forms.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function Forms() {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
+    <div className='w-full h-full flex justify-center items-center bg-gray-100'>
       <iframe 
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdh9PatBTutOboFsrv_A5Kdlrd4801Te9uFRpElXhWAa7sq6Q/viewform?embedded=true" 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdOkNkMJCJUUoqXpocXd6V-P-7Vth4ytbK873-Zfw1R1_beqw/viewform?embedded=true" 
         width="640" 
         height="942" 
         frameborder="0" 
