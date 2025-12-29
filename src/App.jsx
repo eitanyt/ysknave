a52d7b15da007ce6017f72ff656ef4ba739d4b86
eitanyt <125513433+eitanyt@users.noreply.github.com>
2025-12-29 23:43:20 +0200

    Update email address in App component

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                 </svg>
-                <a href="mailto:info@orhadas.edu" className="hover:text-blue-200">yeshivaktananava@gmail.com
+                <a href="mailto:yeshivaktananava@gmail.com" className="hover:text-blue-200">yeshivaktananava@gmail.com
 
                 </a>
               </div>
